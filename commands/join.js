c271699687d7456a28e161efb8f9041d42e7ca15
g k <70080253+gorukuro@users.noreply.github.com>
2021-11-22 01:14:45 +0000

    adding leave cmd and rebrand join also bot activity

--- commands/join.js
+++ commands/join.js
@@ -4,12 +4,13 @@ module.exports = {
   exec: function(msg, args, bot){
     if(!msg.member) return msg.channel.send("cant ixecute in `DM Channel`")
     const { voiceChannel } = msg.member;
-    if(!voiceChannel) return msg.channel.send("make sure u did join voice channel or maybe im not join ur server!")
+    if(!voiceChannel) return msg.channel.send("...")
 
     const permissions = voiceChannel.permissionsFor(msg.client.user);
-    if(!permissions.has("CONNECT")) return msg.channel.send("im sorry, but i dont have prmss to `CONNECT`");
+    if(!permissions.has("CONNECT")) return msg.channel.send("lah gbs konek");
 
     // join voiceChannel
+    bot.user.setActivity(`Tidur di ${voiceChannel.name}`);
     return voiceChannel.join();
   }
 }
