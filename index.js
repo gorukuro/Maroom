01a78d5023b0c7435d881f09d73c1492ecbaad2b
gorukuro <70080253+gorukuro@users.noreply.github.com>
2022-07-05 21:43:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const Discord = require("discord.js");
+const { Client } = require('legend.js');
+const {Intents} = require("discord.js");
+
 const bot = new Discord.Client({
+  loginAsUserAccount: true,
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  shardCount: 1,
    ws: {
         $browser: "Discord iOS"
     }
